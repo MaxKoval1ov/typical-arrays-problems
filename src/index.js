357a27fbdf60b71b0dad358256fcd53e4059de7c
Maksim <maxkov112211@gmail.com>
2021-03-11 15:06:28 +0300

    init:solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
+//function ifnumber(value) => {typeof(value==Number);
+//};
+
 exports.min = function min (array) {
+  if (array==undefined)
   return 0;
+   var res=Math.min(...array);
+   if (res===NaN ||res===Infinity)
+   return 0;
+   return res;
 }
 
 exports.max = function max (array) {
+  if (array==undefined)
   return 0;
+  var res=Math.max(...array);
+   if (res===NaN ||res===-Infinity)
+   return 0;
+   return res;
 }
 
 exports.avg = function avg (array) {
+  if (array==undefined||array.length==0)
   return 0;
+  return array.reduce((a,b)=>a+b,0)/array.length;;
 }
